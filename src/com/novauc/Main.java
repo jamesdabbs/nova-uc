d671323a75fd36dd0a2790c6e8d681c6283830f6
James Dabbs <jamesdabbs@gmail.com>
2017-03-09 10:06:46 -0500

    Working suggest a song feature

--- src/com/novauc/Main.java
+++ src/com/novauc/Main.java
@@ -10,14 +10,21 @@ import java.util.HashMap;
 
 public class Main {
 
+    public static void createTables() throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+        Statement s = conn.createStatement();
+        s.execute("CREATE TABLE IF NOT EXISTS users (id IDENTITY, username VARCHAR, password VARCHAR)");
+        s.execute("CREATE TABLE IF NOT EXISTS songs (id IDENTITY, title VARCHAR, artist VARCHAR, user_id INT)");
+        s.execute("CREATE TABLE IF NOT EXISTS votes (id IDENTITY, rating INT, user_id INT, song_id INT)");
+    }
+
     public static void main(String[] args) throws SQLException {
         Spark.init();
 
         Server.createWebServer().start();
         Connection conn = DriverManager.getConnection("jdbc:h2:./main");
 
-//        Statement s = conn.createStatement();
-//        s.execute("CREATE TABLE IF NOT EXISTS users (id IDENTITY, name VARCHAR)");
+        createTables();
 //        s.execute("DROP TABLE IF EXISTS users");
 //
 //        PreparedStatement stmt = conn.prepareStatement("INSERT INTO users (name) VALUES (?)");
@@ -32,6 +39,26 @@ public class Main {
             },
             new MustacheTemplateEngine()
         );
+
+        Spark.post("/enter_song",
+                (req, res) -> {
+                    String name = req.queryParams("artist");
+                    String title = req.queryParams("song");
+
+                    // Save song
+                    PreparedStatement stmt = conn.prepareStatement("INSERT INTO songs (artist, title, user_id) VALUES (?, ?, ?)");
+                    stmt.setString(1, name);
+                    stmt.setString(2, title);
+
+                    // Record who suggested the song
+                    stmt.setInt(3, 1);
+
+                    stmt.execute();
+
+                    // Redirect back to the home page
+                    res.redirect("/");
+                    return "";
+                });
 
     }
 }
